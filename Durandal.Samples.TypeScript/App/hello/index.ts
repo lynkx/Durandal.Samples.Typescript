ed39cf41ff18ec0577dcd07f202fe0b21a75624e
Milos Ostojic <ostojic.milos>
2014-11-21 17:16:36 +0100

    Initial commit

---
+++ Durandal.Samples.TypeScript/App/hello/index.ts
@@ -0,0 +1,37 @@
+﻿ import app = require('durandal/app')
+ import system = require('durandal/system')
+ import ko = require('knockout')
+
+var name = ko.observable();
+var canSayHello = () => {
+    return name() ? true : false;
+}
+var singleton = {
+    displayName: 'What is your name?',
+    name: name,
+    sayHello: () => {
+        app.showMessage('Hello ' + name() + '!', 'Greetings');
+    },
+    canSayHello: canSayHello,
+    activate: () => {
+        system.log('Lifecycle : activate : hello');
+    },
+    binding: () => {
+        system.log('Lifecycle : binding : hello');
+        return { cacheViews: false }; //cancels view caching for this module, allowing the triggering of the detached callback
+    },
+    bindingComplete: () => {
+        system.log('Lifecycle : bindingComplete : hello');
+    },
+    attached: (view, parent) => {
+        system.log('Lifecycle : attached : hello');
+    },
+    compositionComplete: view => {
+        system.log('Lifecycle : compositionComplete : hello');
+    },
+    detached: view => {
+        system.log('Lifecycle : detached : hello');
+    }
+
+}
+ export = singleton
---
+++ Durandal.Samples.TypeScript/App/main.js
@@ -0,0 +1,39 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions',
+        'knockout': '../Scripts/knockout-3.2.0',
+        'bootstrap': '../Scripts/bootstrap',
+        'jquery': '../Scripts/jquery-2.1.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        }
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Samples';
+
+    //specify which plugins to install and their configuration
+    app.configurePlugins({
+        router: true,
+        dialog: true,
+        widget: {
+            kinds: ['expander']
+        }
+    });
+
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('shell');
+    });
+});
