2f68fc066d76d54c13d822e078c33e3e8cb0cac6
lynkx <ostojic.milos@gmail.com>
2014-11-24 18:11:58 +0100

    Added ko/betterList module

---
+++ Durandal.Samples.TypeScript/App/ko/betterList/index.ts
@@ -0,0 +1,27 @@
+﻿import ko = require('knockout');
+var itemToAdd = ko.observable("");
+var allItems = ko.observableArray(["Fries", "Eggs Benedict", "Ham", "Cheese"]);
+var selectedItems = ko.observableArray(["Ham"]);
+
+var vm = {
+    itemToAdd: itemToAdd,
+    allItems: allItems,
+    selectedItems: selectedItems,
+    addItem: () => {
+        var value = itemToAdd();
+
+        if (value != "" && allItems.indexOf(value) < 0) { // Prevent blanks and duplicates
+            allItems.push(value);
+        }
+
+        itemToAdd(""); // Clear the text box
+    },
+    removeSelected: () => {
+        allItems.removeAll(selectedItems());
+        selectedItems([]); // Clear selection
+    },
+    sortItems: () => {
+        allItems.sort();
+    }
+}
+export = vm;
