1f4fce515e36515f9e5a50687e134e25aca4c532
Milos Ostojic <ostojic.milos>
2014-11-24 17:42:13 +0100

    Added ko/simpleList module

---
+++ Durandal.Samples.TypeScript/App/ko/simpleList/index.ts
@@ -0,0 +1,19 @@
+﻿import ko = require('knockout');
+
+var items = ko.observableArray(["Alpha", "Beta", "Gamma"]);
+var itemToAdd = ko.observable("");
+
+
+var vm = {
+    items: items,
+    itemToAdd: itemToAdd,
+    addItem: () => {
+        if (itemToAdd() != '') {
+            items.push(itemToAdd()); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+            itemToAdd(''); // Clears the text box, because it's bound to the "itemToAdd" observable
+        };
+    }
+
+};
+
+export = vm;
