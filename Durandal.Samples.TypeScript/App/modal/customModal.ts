fe12260dc10d65dc385475087569ac237493bf19
Milos Ostojic <ostojic.milos>
2014-11-24 13:20:32 +0100

    Added customModal module

---
+++ Durandal.Samples.TypeScript/App/modal/customModal.ts
@@ -0,0 +1,15 @@
+﻿import dialog = require('plugins/dialog');
+import ko = require('knockout');
+
+class CustomModal {
+    input = ko.observable();
+    ok = () => {
+        dialog.close(this, this.input());
+    }
+    canDeactivate = () => dialog.showMessage('Are you sure that\'s your favorite color?', 'Just Checking...', ['Yes', 'No']);
+    static show = () => dialog.show(new CustomModal());
+
+}
+
+export = CustomModal
+
---
+++ Durandal.Samples.TypeScript/App/modal/index.js
@@ -0,0 +1,13 @@
+﻿define(["require", "exports", 'durandal/app', './customModal'], function(require, exports, app, CustomModal) {
+    var instance = {
+        showCustomModal: function () {
+            CustomModal.show().then(function (response) {
+                app.showMessage('You answered "' + response + '".');
+            });
+        }
+    };
+
+    
+    return instance;
+});
+//# sourceMappingURL=index.js.map
