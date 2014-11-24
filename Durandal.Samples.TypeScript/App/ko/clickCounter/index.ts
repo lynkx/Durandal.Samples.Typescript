52ed92fe2eee1b9a859c8fc389f5ff1e8b52a785
Milos Ostojic <ostojic.milos>
2014-11-24 17:36:52 +0100

    Added ko/clickCounter module

---
+++ Durandal.Samples.TypeScript/App/ko/clickCounter/index.ts
@@ -0,0 +1,13 @@
+﻿import ko = require('knockout');
+var counter = ko.observable(0),
+    limiter = ko.computed(() => counter() >= 3);
+var vm = {
+    numberOfClicks: counter,
+    hasClickedTooManyTimes: limiter,
+    registerClick: () => { counter(counter() + 1); },
+    resetClicks: () => { counter(0); }
+
+};
+
+export = vm;
+ 
