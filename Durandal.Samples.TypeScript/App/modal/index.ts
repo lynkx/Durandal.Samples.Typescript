b517ae99f7e1c917713ebe868fb56bc4c5a987f9
Milos Ostojic <ostojic.milos>
2014-11-24 13:19:28 +0100

    Added customModal module

---
+++ Durandal.Samples.TypeScript/App/modal/index.ts
@@ -0,0 +1,12 @@
+﻿import app = require('durandal/app');
+import CustomModal = require('./customModal');
+
+var instance = {
+    showCustomModal: () => {
+        CustomModal.show().then(response => {
+            app.showMessage('You answered "' + response + '".');
+        });
+    }
+}
+
+export = instance
